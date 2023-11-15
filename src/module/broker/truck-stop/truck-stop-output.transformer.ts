eacda95d17d5a31c07d2f57c5d2b34bac6e09aca
hieu.pham <hieu.pham@coderpush.com>
2023-11-15 18:18:26 +0700

    return empty data if catch error truckstop

--- src/module/broker/truck-stop/truck-stop-output.transformer.ts
+++ src/module/broker/truck-stop/truck-stop-output.transformer.ts
@@ -8,6 +8,7 @@ import {
 import { Loc } from '@core/util/loc';
 import { MapboxService } from '@module/broker/service/mapbox.service';
 import { PriceService } from '@module/broker/service/price.service';
+import { Logging } from '@core/logger/logging.service';
 
 @Injectable()
 export class TruckStopOutputTransformer {
@@ -33,7 +34,7 @@ export class TruckStopOutputTransformer {
         destinationState: load.DestinationCountry
       };
       const deliveryInfo = await this.calculateInfo(input);
-
+      if (!deliveryInfo) continue;
       loadModel.pickupStop = {
         address: deliveryInfo.originalPlaceName,
         coordinates: {
@@ -82,66 +83,75 @@ export class TruckStopOutputTransformer {
   async calculateInfo(
     input: TruckStopDeliveryAddressInfo
   ): Promise<TruckStopDeliveryAddressInfoResponse> {
-    const origin = await this.mapboxService.searchAddress(
-      input.originCity,
-      input.originState,
-      input.originCountry
-    );
-    const destination = await this.mapboxService.searchAddress(
-      input.destinationCity,
-      input.destinationState,
-      input.destinationCountry
-    );
-    if (!origin?.center || !destination.center) {
-      return null;
-    }
-    const waypoints = [
-      {
-        coordinates: origin.center
-      },
-      {
-        coordinates: destination.center
-      }
-    ];
-
-    const directionsRequest = {
-      waypoints
-    };
-
-    // return {
-    //   originalCoordinates: origin.center,
-    //   originalPlaceName: origin.place_name,
-    //   destinationCoordinates: destination.center,
-    //   destinationPlaceName: destination.place_name,
-    //   estimationDistance: 0,
-    //   estimationDurations: 0,
-    //   estimationAmount: 0
-    // };
-
-    let response;
     try {
-      response = await this.mapboxService.directionsService.getDirections(directionsRequest).send();
-    } catch (e) {
-      throw new InternalServerErrorException(e);
+      const origin = await this.mapboxService.searchAddress(
+        input.originCity,
+        input.originState,
+        input.originCountry
+      );
+      const destination = await this.mapboxService.searchAddress(
+        input.destinationCity,
+        input.destinationState,
+        input.destinationCountry
+      );
+      if (!origin?.center || !destination.center) {
+        return null;
+      }
+      const waypoints = [
+        {
+          coordinates: origin.center
+        },
+        {
+          coordinates: destination.center
+        }
+      ];
+
+      const directionsRequest = {
+        waypoints
+      };
+
+      // return {
+      //   originalCoordinates: origin.center,
+      //   originalPlaceName: origin.place_name,
+      //   destinationCoordinates: destination.center,
+      //   destinationPlaceName: destination.place_name,
+      //   estimationDistance: 0,
+      //   estimationDurations: 0,
+      //   estimationAmount: 0
+      // };
+
+      // let response;
+
+      const response = await this.mapboxService.directionsService
+        .getDirections(directionsRequest)
+        .send();
+      const route = response.body.routes[0];
+
+      const distance = Loc.metersToMiles(route.distance); // (miles)
+      const durations = route.duration / 60; // (minutes)
+      // deadheadMiles is the distance from the origin to the pickup point (no load)
+      // Todo: getDirection from origin to pickup point, then get distance
+      const deadheadMiles = 0;
+      const amount = this.priceService.getAmount(distance, deadheadMiles);
+      const result: TruckStopDeliveryAddressInfoResponse = {
+        originalCoordinates: origin.center,
+        originalPlaceName: origin.place_name,
+        destinationCoordinates: destination.center,
+        destinationPlaceName: destination.place_name,
+        estimationDistance: distance,
+        estimationDurations: durations,
+        estimationAmount: amount
+      };
+
+      return result;
+    } catch (err) {
+      Logging.error(
+        `[TruckStop] Search Multiple Details Available Loads got error`,
+        err.response?.data ?? err
+      );
+
+      return null;
+      // throw new InternalServerErrorException(e);
     }
-    const route = response.body.routes[0];
-
-    const distance = Loc.metersToMiles(route.distance); // (miles)
-    const durations = route.duration / 60; // (minutes)
-    // deadheadMiles is the distance from the origin to the pickup point (no load)
-    // Todo: getDirection from origin to pickup point, then get distance
-    const deadheadMiles = 0;
-    const amount = this.priceService.getAmount(distance, deadheadMiles);
-    const result: TruckStopDeliveryAddressInfoResponse = {
-      originalCoordinates: origin.center,
-      originalPlaceName: origin.place_name,
-      destinationCoordinates: destination.center,
-      destinationPlaceName: destination.place_name,
-      estimationDistance: distance,
-      estimationDurations: durations,
-      estimationAmount: amount
-    };
-
-    return result;
   }
 }
